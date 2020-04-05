bb8bbf3da3fdc9686076c272397646512e58784e
Kamy <kamy.leeret@gmail.com>
2020-04-05 12:12:09 -0600

    Recreate Repository

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  clientId: process.env.SPOTIFY_ID,
+  clientSecret: process.env.SPOTIFY_SECRET
+};
+
+
---
+++ liri.js
@@ -0,0 +1,66 @@
+
+//https://www.npmjs.com/package/spotify-web-api-node//
+require("dotenv").config();
+
+console.log(document.body.rootNode);
+
+//Set Variables
+var request = require("request");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('spotify-web-api-node');
+var spotifyQry = new Spotify(keys.spotify);
+
+
+//vars to capture input process.argv
+var userQry = process.argv[2]; 
+var qryParameter = process.argv[3];
+
+//Execute function
+SpotifyQry(userQry, qryParameter);
+
+function SpotifyQry (userQry, qryParameter) {
+  switch (userQry) {
+    case 'concert-this':
+      concertThisInfo(qryParameter);
+      break;
+    case 'song-this':
+      songThisInfo(qryParameter);
+      break;
+    case 'movie-this':
+      movieThisInfo(qryParameter);
+      break;
+    case 'do-what-it-says':
+      whatThisInfo(); 
+      break;
+    default: 
+      console.log("Invalid Option. Please type any of the following options: \nconcert-this \nsong-this \nmovie-this \ndo-what-it-says")
+}
+}
+
+//function for Spotify
+
+function showSpotify(qryParameter) {
+  if (qryParameter===undefined) {
+    qryParameter="River"; //song default
+  }
+  spotifyQry.search( 
+    {
+      type: "track",
+      query: qryParameter
+    }, 
+    function (err, data) {
+      if(err) {
+        console.log("error" + err)
+        return;
+      }
+      var songs = data.tracks.items;
+
+      for (var i=0; i < songs.length; i++) {
+          console.log("SONG INFORMATION");
+          fs.appendFileFileSync("log.text", "Song Info\n");
+          console.log(i);
+      }
+    }
+  )
+}
